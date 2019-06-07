d66265eec1568e1c645eca54730101b18062ed8a
GeorgesKebabadie <georges@kebabadie.corp>
2019-06-07 13:26:03 +0200

    Suppression des simples quotes et rempalcement par double quotes dans json envoyé à NERthon

--- scrapython.py
+++ scrapython.py
@@ -2,38 +2,32 @@ import json
 import logging
 import os
 import time
-from argparse import ArgumentParser
 
 from crochet import setup
 from kafka import KafkaConsumer
 from kafka import KafkaProducer
 from scrapy.crawler import CrawlerProcess
 from scrapy.settings import Settings
-from twisted.internet import reactor
 from twisted.internet.defer import inlineCallbacks
 
 from common import settings as my_settings
 from common.spiders import spiderthon
 
-
-
-#recuperation variable d'env
+# recuperation variable d'env
 kafka_endpoint = str(os.environ['KAFKA_IP']) + ":" + str(os.environ['KAFKA_PORT'])
-topic_in=str(os.environ['TOPIC_IN'])
-topic_out=str(os.environ['TOPIC_OUT'])
-complexity= int(os.environ['COMPLEXITY'])
-debug_level=os.environ["DEBUG"]
-
+topic_in = str(os.environ['TOPIC_IN'])
+topic_out = str(os.environ['TOPIC_OUT'])
+complexity = int(os.environ['COMPLEXITY'])
+debug_level = os.environ["DEBUG"]
 
 # gére le reactor de scrapy dans un thread différent
 setup()
 
-
 logging.basicConfig(level=logging.INFO)
 if debug_level == "DEBUG":
-       logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.DEBUG)
 elif debug_level == "INFO":
-        logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.INFO)
 elif debug_level == "WARNING":
     logging.basicConfig(level=logging.WARNING)
 elif debug_level == "ERROR":
@@ -41,8 +35,8 @@ elif debug_level == "ERROR":
 elif debug_level == "CRITICAL":
     logging.basicConfig(level=logging.CRITICAL)
 
-
 scrapython_acq_ended = False
+
 
 @inlineCallbacks
 # Crawl la liste des urls de page internet donnée selon les settings dans spiderthon.py
@@ -56,7 +50,6 @@ def loop_urls(urls):
 # Suppression de l'ancien fichier de récupération des résultats du crawl des urls fournies
 # Ce fichier est en effet créé à chaque run du script,
 # ou complété (et on ne veut pas que les données deux deux scripts se suivent)
-
 
 results_file_exists = os.path.isfile("results.json")
 if results_file_exists:
@@ -86,40 +79,42 @@ for message in consumer:
     scrapyResults = []
     complexityLevel = 1
     if 'complexity' not in message or (int(message['complexity']) <= int(complexity)):
-            for i in range(len(message['url'])):
-                urlList = []
-                urlList.append(message['url'][i])
+        for i in range(len(message['url'])):
+            urlList = []
+            urlList.append(message['url'][i])
 
-                loop_urls(urlList)
+            loop_urls(urlList)
 
-                while (scrapython_acq_ended == False):
-                    time.sleep(1)
-                scrapyResult = {}
-                if "results.json":  # TODO: changer le mode de sortie en stream
-                    textString = ''
+            while (scrapython_acq_ended == False):
+                time.sleep(1)
+            scrapyResult = {}
+            if "results.json":  # TODO: changer le mode de sortie en stream
+                textString = ''
 
-                    with open("results.json", 'r') as f:
-                        data = json.load(f)
-                        for text in data:
+                with open("results.json", 'r') as f:
+                    data = json.load(f)
+                    for text in data:
 
-                            if next(iter(text)) == 'text':
-                                textString = textString + " " + text['text']
-                            elif next(iter(text)) == 'urls':
-                                if int(complexity) > 1:
-                                    if 'complexity' not in message:
-                                        complexityLevel = 2
-                                    else:
-                                        complexityLevel = int(message['complexity']) + 1
-                                #renvoi dans topic in pour retraitement par scrapython
-                                producer.send(
-                                    topic_in,
-                                    value={'idBio': message['idBio'], 'url': message['url'], 'complexity': complexityLevel})
+                        if next(iter(text)) == 'text':
+                            textString = textString + " " + text['text']  # TODO y remplacer les ' par "
+                        elif next(iter(text)) == 'urls':
+                            if int(complexity) > 1:
+                                if 'complexity' not in message:
+                                    complexityLevel = 2
+                                else:
+                                    complexityLevel = int(message['complexity']) + 1
+                            # renvoi dans topic in pour retraitement par scrapython
 
-                    scrapyResult.update([('url', urlList[0]), ('content', textString)])
-                    scrapyResults.append(scrapyResult)
+                            producer.send(
+                                topic_in,
+                                value={'idBio': message['idBio'], 'url': message['url'], 'complexity': complexityLevel})
 
-                os.remove("results.json")
+                scrapyResult.update([('url', urlList[0]), ('content', textString)])
+                scrapyResults.append(scrapyResult)
 
-            producer.send(
-                topic_out,
-                value={'idBio': message['idBio'], 'scrapyResults': scrapyResults})
+            os.remove("results.json")
+
+        value_to_send = json.dumps({'idBio': message['idBio'], 'scrapyResults': scrapyResults})
+
+        producer.send(
+            topic_out, value=value_to_send)
